e1a4830f2bea43981b4b5853c2edbfb81ffb45dd
suyashLade <suyashlade@gmail.com>
2023-07-29 11:14:41 +0530

    added target blank to links

--- app/[userName]/page.js
+++ app/[userName]/page.js
@@ -1,34 +1,33 @@
-
 import { Getlinks } from "@/api/simpleLinks/simplelinksServices";
 import styles from "./link.module.css";
 import EventsChip from "@/components/events/eventsChip";
 import Heading from "@/components/common/Heading/Heading";
 
-
 export default async function Page({ params }) {
   const Links = await Getlinks(params.userName);
 
-
   return (
     <>
-    <head>
-      <title>{params.userName}</title>
-    </head>
-     <body>
-     <div className={styles.mainComp}>
-        <div className={styles.headingComp}>
-          <Heading size="md" cplor="var(--black-1)">
-            {" "}
-            <b>@</b> {params.userName}
-          </Heading>
+      <head>
+        <title>{params.userName}</title>
+      </head>
+      <body>
+        <div className={styles.mainComp}>
+          <div className={styles.headingComp}>
+            <Heading size="md" cplor="var(--black-1)">
+              {" "}
+              <b>@</b> {params.userName}
+            </Heading>
+          </div>
+          <div className={styles.links}>
+            {Links.map((data, index) => {
+              return (
+                <EventsChip key={index} url={data.url} title={data.name} />
+              );
+            })}
+          </div>
         </div>
-        <div className={styles.links}>
-          {Links.map((data, index) => {
-            return <EventsChip key={index} url={data.url} title={data.name} />;
-          })}
-        </div>
-      </div>
-     </body>
+      </body>
     </>
   );
 }
--- components/events/eventsChip.js
+++ components/events/eventsChip.js
@@ -7,18 +7,28 @@ export default function EventsChip({ url, title, size = "lg" }) {
   if (size === "lg") {
     return (
       <div className={styles.mainComp}>
-        <div className={styles.Chip}>
-          <Link href={url} style={{ textDecoration: "none" }} prefetch={true} target="_blank">
+        <Link
+          href={url}
+          style={{ textDecoration: "none" }}
+          prefetch={true}
+          target="_blank"
+        >
+          <div className={styles.Chip}>
             <Text size="lg">{title}</Text>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     );
   }
   if (size === "md") {
     return (
       <div className={styles.mainComp}>
-        <Link href={url} style={{ textDecoration: "none" }} prefetch={true}>
+        <Link
+          href={url}
+          style={{ textDecoration: "none" }}
+          prefetch={true}
+          target="_blank"
+        >
           <div className={styles.Chipmd}>
             <Text size="lg">{title}</Text>
           </div>
